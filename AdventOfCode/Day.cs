7079087699b602b17708d7f925c09e8c5a42c601
Tobias Balzano <tobias@balzano.se>
2020-12-02 17:02:41 +0100

    initial commit

---
+++ AdventOfCode/Day.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public abstract class Day
+    {
+        public static Stopwatch Stopwatch;
+        public static void Run(int day, Action action)
+        {
+            Console.WriteLine($"Day {day}:");
+            Stopwatch = new Stopwatch();
+            Stopwatch.Start();
+            action.Invoke();
+            Stopwatch.Stop();
+            Console.WriteLine($"Elasped seconds: {Stopwatch.Elapsed.TotalSeconds}, milliseconds: {Stopwatch.ElapsedMilliseconds}, ticks: {Stopwatch.ElapsedTicks}");
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose day to run:");
+                foreach (var day in Enumerable.Range(1, 24))
+                {
+                    Console.WriteLine($"Day: {day}");
+                }
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Clear();
+                        Day1.Run();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Day2.Run();
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
